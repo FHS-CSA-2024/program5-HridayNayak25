9616035f23b0b5c334d0ff069af9580682ca0094
HridayNayak25 <hridaynayak8@gmail.com>
2024-08-20 10:11:13 -0700

    Update Program5.java
    code 5 in progress

--- Program5.java
+++ Program5.java
@@ -1,5 +1,42 @@
-//import stuff here?
 
+//import stuff here?
+public class Mileage{
+    public static void main(String[] args){
+        int royaleMiles = 286;
+        int royaleGallons = 9;
+        
+        int koopaKingMiles = 412;
+        int koopaKingGallons = 40;
+        
+        int pipeFrameMiles = 361;
+        int pipeFraemeGallons = 18;
+        
+        int badWagonMiles = 161;
+        int badWagonGallons = 11;
+        
+        double mpg_royale = (double)royaleMiles/royaleGallons;
+        System.out.print(mpg_royale);
+    
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
 //Your code here
 
 //Paste console output below:
@@ -7,3 +44,4 @@
 
 
 */
+
