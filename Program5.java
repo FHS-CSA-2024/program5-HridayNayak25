090edc5b21adac0943644bcc28f96b44a6eb323e
HridayNayak25 <hridaynayak8@gmail.com>
2024-08-21 21:33:29 -0700

    Update Program5.java
    Code 5 draft withe very little decimal point loss

--- Program5.java
+++ Program5.java
@@ -1,21 +1,52 @@
-
-//import stuff here?
 public class Mileage{
     public static void main(String[] args){
         int royaleMiles = 286;
         int royaleGallons = 9;
+        double mpg_royale_1 = (((double)royaleMiles/royaleGallons)*100.0);
+        int mpg_royale_2  = (int) mpg_royale_1;
+        double mpg_royale_round = (((double) mpg_royale_2)/100);
+        double mpg_royale_roundx = mpg_royale_round*10.0;
+        int mpg_royale_roundz = (int)mpg_royale_roundx;
+        double mpg_royale_roundfinal = (double)mpg_royale_roundz/10.0;
+        
+        
         
         int koopaKingMiles = 412;
         int koopaKingGallons = 40;
+        double mpg_koopaKing_1 = (((double)koopaKingMiles/koopaKingGallons)*100.0);
+        int mpg_koopaKing_2  = (int) mpg_koopaKing_1;
+        double mpg_koopaKing_round = (((double) mpg_koopaKing_2)/100);
+        double mpg_koopaKing_roundx = mpg_koopaKing_round*10.0;
+        int mpg_koopaKing_roundz = (int)mpg_koopaKing_roundx;
+        double mpg_koopaKing_roundfinal = (double)mpg_koopaKing_roundz/10.0;
         
         int pipeFrameMiles = 361;
         int pipeFraemeGallons = 18;
+        double mpg_pipeFrame_1 = (((double)pipeFrameMiles/pipeFraemeGallons)*100.0);
+        int mpg_pipeFrame_2  = (int) mpg_pipeFrame_1;
+        double mpg_pipeFrame_round = (((double) mpg_pipeFrame_2)/100);
+        double mpg_pipeFrame_roundx = mpg_pipeFrame_round*10.0;
+        int mpg_pipeFrame_roundz = (int)mpg_pipeFrame_roundx;
+        double mpg_pipeFrame_roundfinal = (double)mpg_pipeFrame_roundz/10.0;
         
         int badWagonMiles = 161;
         int badWagonGallons = 11;
+        double mpg_badWagon_1 = (((double)badWagonMiles/badWagonGallons)*100.0);
+        int mpg_badWagon_2  = (int) mpg_badWagon_1;
+        double mpg_badWagon_round = (((double) mpg_badWagon_2)/100);
+        double mpg_badWagon_roundx = mpg_badWagon_round*10.0;
+        int mpg_badWagon_roundz = (int)mpg_badWagon_roundx;
+        double mpg_badWagon_roundfinal = (double)mpg_badWagon_roundz/10.0;
         
-        double mpg_royale = (double)royaleMiles/royaleGallons;
-        System.out.print(mpg_royale);
+        
+        System.out.println("Royale average mileage: " + mpg_royale_roundfinal);
+        System.out.println("KoopaKing average mileage: " + mpg_koopaKing_roundfinal);
+        System.out.println("PipeFrame average mileage: " + mpg_pipeFrame_roundfinal);
+        System.out.println("BadWagon average mileage: " + mpg_badWagon_roundfinal);
+        
+        
+        
+       
     
         
         
@@ -44,4 +75,3 @@ public class Mileage{
 
 
 */
-
